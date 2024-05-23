dd595cab769777fd7eed1d6b96c06dc161af2e00
fadyawan <muhammad.adyawan@kurniawan.co.uk>
2024-05-23 10:35:29 +0100

    working, need to find out what is wrong with pool creation and access

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express');
 
-const app = express();
+// const app = express();
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-    host: 'CHINGCHANGCENTRAL',
+    host: 'localhost',
     user: 'root',
     password: 'F@WaT2012',
     database: 'hotelservice',
@@ -13,24 +13,31 @@ const pool = mysql.createPool({
     queueLimit: 0
 });
 
-pool.getConnection((err, connection) => {
-    if (err) {
-        console.error('Error connecting to database:', err);
-        return;
-    }
-    console.log('Connected to MySQL database successfully!');
-    connection.release();
-});
+// pool.getConnection((err, connection) => {
+//     if (err) {
+//         console.error('Error connecting to database:', err);
+//         return;
+//     }
+//     console.log('Connected to MySQL database successfully!');
+//     connection.release();
+// });
 
-pool.end((err) => {
+pool.query(`SELECT * FROM hotelservice.location;`, (err, result, fields) => {
     if (err) {
-        console.error('Error closing pool:', err);
-        return;
+        return console.log(err)
     }
-    console.log('Pool closed');
-});
+    return console.log(result)
+})
 
-const port = 3000;
-app.listen(port, () => {
-    console.log(`Server is listening on port ${port}`);
-});
+// pool.end((err) => {
+//     if (err) {
+//         console.error('Error closing pool:', err);
+//         return;
+//     }
+//     console.log('Pool closed');
+// });
+
+// const port = 3000;
+// app.listen(port, () => {
+//     console.log(`Server is listening on port ${port}`);
+// });
