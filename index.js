b4b98ee109253e7619c4e957721931f8f7ff0a7f
fadyawan <muhammad.adyawan@kurniawan.co.uk>
2024-05-23 11:17:23 +0100

    unnecessary env use

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ app.get('/', (req, res) => {
 
 app.use('/customers', customerRoutes);
 
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
