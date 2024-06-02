74c885d909560a6d2478aa7d0c63a7c70f0f5492
fadyawan <muhammad.adyawan@kurniawan.co.uk>
2024-06-02 17:49:17 +0100

    what to do next

--- index.js
+++ index.js
@@ -27,3 +27,5 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+// create a service, createBooking, configureHotels, with error handling
